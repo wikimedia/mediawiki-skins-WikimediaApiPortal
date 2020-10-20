2522cda619e8aa3b0dee064a880277d82ce891b6
Cindy Cicalese <cindom@gmail.com>
2020-10-20 09:41:07 -0400

    Make nav bar menus look like user menu.
    Bug: T264763
    Change-Id: Ib1004dc14579772be6ce08179ac3126d420300ae

---
+++ resources/skin.wikimediaapiportal.scripts/primarynavmenu.js
@@ -0,0 +1,48 @@
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * @file
+ */
+$( function () {
+	$( '.primary-nav-menu' ).each( function () {
+		var items = [];
+		$( this ).find( '.primary-nav-menu-item' ).each( function () {
+			items.push(
+				new OO.ui.MenuOptionWidget( {
+					data: this.href,
+					label: this.text
+				} )
+			);
+		} );
+
+		var label = $( this ).children().first().text();
+		var menu = new OO.ui.ButtonMenuSelectWidget( {
+			label: label,
+			indicator: 'down',
+			framed: false,
+			classes: [ 'primary-nav-menu-button' ],
+			menu: {
+				horizontalPosition: 'end',
+				items: items
+			}
+		} );
+
+		menu.getMenu().on( 'choose', function ( menuOption ) {
+			location.replace( menuOption.getData() );
+		} );
+
+		$( this ).children().first().replaceWith( menu.$element );
+	} );
+} );
