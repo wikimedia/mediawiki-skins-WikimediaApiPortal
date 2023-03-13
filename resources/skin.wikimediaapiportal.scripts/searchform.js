60c0c56de9a25483037d9463b2c81d9add6fe921
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2023-03-13 09:34:49 +0000

    build: Updating npm dependencies
    * eslint-config-wikimedia: 0.20.0 → 0.24.0
    * stylelint-config-wikimedia: 0.11.1 → 0.14.0
    
    Change-Id: I9f4c96dbb65818dda20a782afdefd07a3002ce99

--- resources/skin.wikimediaapiportal.scripts/searchform.js
+++ resources/skin.wikimediaapiportal.scripts/searchform.js
@@ -13,6 +13,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
+ * @param d
  * @file
  */
 ( function ( d ) {
