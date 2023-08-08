2db7523d4f1b0c99efe963f20f7ad36eb8bfaf1c
Umherirrender <umherirrender_de.wp@web.de>
2023-08-09 00:12:21 +0200

    Remove @param from @file comment in searchform.js
    Top level function does not get @param
    
    Change-Id: I65f0efee4d37c9125875f658aac898dc2fb0dba9

--- resources/skin.wikimediaapiportal.scripts/searchform.js
+++ resources/skin.wikimediaapiportal.scripts/searchform.js
@@ -13,9 +13,9 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * @param d
  * @file
  */
+
 ( function ( d ) {
 	$( function () {
 		var $element,
